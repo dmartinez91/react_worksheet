9e55886c43d96df86926aeb3d0928c2911991510
Daniel Martinez <danielmartinez@Daniels-MacBook-Pro.local>
2021-08-11 16:34:02 -0400

    refactored nameList Component to print out a list and give apt spacing and use of markers

--- src/components/NamesList/NamesList.jsx
+++ src/components/NamesList/NamesList.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+// const NamesList = (props) => {
+//     return (
+//         <div>
+//             {props.nameList}
+//             <ol className="NamesList"></ol>
+//         </div>
+//     )
+// }
+
 const NamesList = (props) => {
-    return (
+    return ( 
         <div>
-            <ol className="NamesList">{props.nameList}</ol>
+            {console.log(props.nameList)}
+            <ol>
+                {props.nameList.map(function(name, index){
+                   return <li key={index}>{name} </li>;
+                })}
+            </ol>
         </div>
-    )
+     );
 }
+
 
 export default NamesList;
